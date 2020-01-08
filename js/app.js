4fc733a3f33f22fd6978a945a9b92983c84ce201
Johnny Squardo <johnnysquardo@gmail.com>
2020-01-08 08:01:15 -0500

    Updating some old styles.

--- js/app.js
+++ js/app.js
@@ -5,28 +5,34 @@ const unContainer = document.querySelector(".main__profile-username");
 const reposContainer = document.querySelector(".main__profile-repos");
 const urlContainer = document.querySelector(".main__profile-url");
 
-const client_id = 'Iv1.c00799f587c596af';
-const client_secret = '39b794db894f2427d51344183a600393924bb4f6';
+const client_id = "Iv1.c00799f587c596af";
+const client_secret = "39b794db894f2427d51344183a600393924bb4f6";
 
-const fetchUsers = async (user) => {
-    const api_call = await fetch(`https://api.github.com/users/${user}?client_id=${client_id}&client_secret=${client_secret}`);
+const fetchUsers = async user => {
+	const api_call = await fetch(
+		`https://api.github.com/users/${user}?client_id=${client_id}&client_secret=${client_secret}`
+	);
 
-    const data = await api_call.json();
-    return { data: data }
+	const data = await api_call.json();
+	return { data: data };
 };
 
 const showData = () => {
-    fetchUsers(inputValue.value).then((res) => {
-        nameContainer.innerHTML = `Name: <span class="main__profile-value">${res.data.name}</span>`;
+	fetchUsers(inputValue.value).then(res => {
+		nameContainer.innerHTML = `Name: <span class="main__profile-value">${res
+			.data.name}</span>`;
 
-        unContainer.innerHTML = `Username: <span class="main__profile-value">${res.data.login}</span>`;
+		unContainer.innerHTML = `Username: <span class="main__profile-value">${res
+			.data.login}</span>`;
 
-        reposContainer.innerHTML = `Repos: <span class="main__profile-value">${res.data.public_repos}</span>`;
+		reposContainer.innerHTML = `Repos: <span class="main__profile-value">${res
+			.data.public_repos}</span>`;
 
-        urlContainer.innerHTML = `URL: <span class="main__profile-value">${res.data.html_url}</span>`;
-    })
+		urlContainer.innerHTML = `URL: <a><span class="main__profile-value">${res
+			.data.html_url}</span></a>`;
+	});
 };
 
-searchButton.addEventListener('click', () => {
-    showData();
+searchButton.addEventListener("click", () => {
+	showData();
 });
